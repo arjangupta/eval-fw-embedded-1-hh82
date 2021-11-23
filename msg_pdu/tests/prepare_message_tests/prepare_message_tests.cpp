88b343f008709db33876018fe3d54041ba45f6cb
Arjan Gupta <arjangupta95@gmail.com>
2021-11-23 00:37:11 -0600

    Write the happy path test for prepare message

--- msg_pdu/tests/prepare_message_tests/prepare_message_tests.cpp
+++ msg_pdu/tests/prepare_message_tests/prepare_message_tests.cpp
@@ -1,24 +1,71 @@
+extern "C"
+{
 #include "msg_pdu.h"
+}
 #include <gtest/gtest.h>
 #include <iostream>
 
 namespace testing_msg_pdu
 {
 
+const uint16_t getRandomTwoBytes()
+{
+    return rand() % 65536;
+}
+
+const uint8_t getRandomByte()
+{
+    return rand() % 256;
+}
+
 TEST(prepare_message_tests, happy_path_test1)
 {
     // Form the message body
-    const uint8_t message_body_length = rand() % 256;
-    std::cout << "Message body length generated as: " << message_body_length << std::endl;
+    const uint8_t message_body_length = getRandomByte();
+    std::cout << "Message body length generated as: " << (int)message_body_length << std::endl;
     std::vector<uint8_t> pdu_body_vector;
     pdu_body_vector.reserve(message_body_length);
     for (size_t i = 0; i < message_body_length; i++)
     {
-        uint8_t random_byte = rand() % 256; // Some value 0-255
+        pdu_body_vector.push_back(getRandomByte());
     }
 
-    // Destination addr 453, source addr 22, body len 12, 
-    Msg_PDU pdu1 = { 453, 22, message_body_length,  };
+    // Destination addr 453, source addr 22, body len 12,
+    const uint16_t destination_address = getRandomTwoBytes();
+    uint16_t source_address = getRandomTwoBytes();
+    while (destination_address == source_address)
+    {
+        source_address = getRandomTwoBytes();
+    }
+    
+    // Initialize pdu
+    Msg_PDU test_pdu1;
+    test_pdu1.dest_addr = destination_address;
+    test_pdu1.src_addr = source_address;
+    test_pdu1.body_len = message_body_length;
+    memcpy(test_pdu1.body, pdu_body_vector.data(), message_body_length);
+
+    // Call function for test
+    const int buffer_alloc_size = 1+1+2+2+1+message_body_length+2;
+    std::cout << "buffer alloc size is " << buffer_alloc_size << std::endl;
+    uint8_t* output_buffer = (uint8_t*)malloc(buffer_alloc_size);
+    prepareMessage(&test_pdu1, output_buffer, buffer_alloc_size);
+
+    // Create expectation buffer
+    uint8_t expected_buffer[buffer_alloc_size] = {
+        Msg_PDU_PREAMBLE0,
+        Msg_PDU_PREAMBLE1,
+        source_address,
+        destination_address,
+        message_body_length,
+    };
+    memcpy((expected_buffer+1+1+2+2+1), pdu_body_vector.data(), message_body_length);
+    expected_buffer[1+1+2+2+1+message_body_length] = calculate_checksum(pdu_body_vector.data(), message_body_length);
+
+    EXPECT_EQ(memcmp(expected_buffer, output_buffer, buffer_alloc_size), 0);
+
+    // Free memory
+    delete output_buffer;
 }
 
 } // namespace testing_msg_pdu
